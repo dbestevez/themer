d5699cd122500ced9e065520cc1bfe98d5968ad1
Diego Blanco Estévez <dbestevez@protonmail.com>
2020-11-16 00:20:36 +0100

    Extract colors from themes

---
+++ colors/dracula.sh
@@ -0,0 +1,35 @@
+export color0="#232734"
+export color1="#c74950"
+export color2="#43c46a"
+export color3="#cdd778"
+export color4="#a48bf7"
+export color5="#d96aa9"
+export color6="#6272a4"
+export color7="#bfbfbf"
+export color8="#44475a"
+export color9="#ff5555"
+export color10="#50fa7b"
+export color11="#f1fa8c"
+export color12="#bd93f9"
+export color13="#ff79c6"
+export color14="#8be9fd"
+export color15="#f8f8f2"
+export color16="#20202a"
+export color17="#d4d7d6"
+export color18="#8a553f"
+export color19="#8ec43d"
+export color20="#3d556d"
+export color21="#43a5d5"
+export color22="#3a4749"
+export color23="#ff7944"
+
+# Flat theme
+export flat_bg=$color16
+export flat_bg_hl=$color8
+export flat_bg_hl_val=8
+export flat_bg_mt=$color16
+export flat_fg=$color6
+export flat_fg_hl=$color4
+export flat_fg_hl_val=4
+export flat_fg_mt=$color0
+export flat_tmux_fg_hl="colour4"
---
+++ colors/nord.sh
@@ -0,0 +1,36 @@
+# Nord theme
+export color0="#3b4252"
+export color1="#bf616a"
+export color2="#a3be8c"
+export color3="#ebcb8b"
+export color4="#81a1c1"
+export color5="#b48ead"
+export color6="#88c0d0"
+export color7="#d8dee9"
+export color8="#434c5e"
+export color9="#bf616a"
+export color10="#a3be8c"
+export color11="#ebcb8b"
+export color12="#81a1c1"
+export color13="#b48ead"
+export color14="#8fbcbb"
+export color15="#eceff4"
+export color16="#2e3440"
+export color17="#dbdcdd"
+export color18="#8a553f"
+export color19="#8ec43d"
+export color20="#3b758c"
+export color21="#43a5d5"
+export color22="#555555"
+export color23="#db7b55"
+
+# Flat theme
+export flat_bg=$color16
+export flat_bg_hl=$color8
+export flat_bg_hl_val=8
+export flat_bg_mt=$color16
+export flat_fg=$color7
+export flat_fg_hl=$color4
+export flat_fg_hl_val=4
+export flat_fg_mt=$color0
+export flat_tmux_fg_hl="colour4"
