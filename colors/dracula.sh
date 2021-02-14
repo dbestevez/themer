d7cd375e0303684519f201a52f98f592793b5607
Diego Blanco Estévez <dbestevez@protonmail.com>
2021-02-14 17:49:26 +0100

    Update colors for future theme

--- colors/dracula.sh
+++ colors/dracula.sh
@@ -58,14 +58,12 @@ export flat_tmux_nm_fg="colour8"
 export flat_vim_hl_fg=4
 export flat_vim_nm_fg=12
 
-# Tabular theme
-export future_bg=$color16
-export future_bg_hl=$color8
-export future_bg_hl_val=8
-export future_bg_mt=$color16
-export future_cursor=$color6
-export future_fg=$color7
-export future_fg_hl=$color4
-export future_fg_hl_val=4
-export future_fg_mt=$color8
-export future_tmux_fg_hl="colour4"
+# Future theme
+export future_hl_bg=$color8
+export future_hl_fg=$color4
+export future_mt_bg=$color16
+export future_mt_fg=$color8
+export future_nm_bg=$color16
+export future_nm_fg=$color7
+export future_termite_cursor=$color4
+export future_tmux_hl_fg="colour4"
