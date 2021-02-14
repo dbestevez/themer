c7ef5b973e97b9f2f978ec3e841678b9035edad9
Diego Blanco Estévez <dbestevez@protonmail.com>
2021-02-14 18:14:41 +0100

    Update colors for square theme

--- colors/seti.sh
+++ colors/seti.sh
@@ -67,3 +67,9 @@ export future_nm_bg=$color16
 export future_nm_fg=$color7
 export future_termite_cursor=$color4
 export future_tmux_hl_fg="colour4"
+
+# Square theme
+export square_nm_bg=$color16
+export square_nm_fg=$color7
+export square_hl_bg=$color0
+export square_termite_cursor=$color4
--- colors/solarized.sh
+++ colors/solarized.sh
@@ -84,3 +84,9 @@ export future_nm_bg=$color8
 export future_nm_fg=$color7
 export future_termite_cursor=$color6
 export future_tmux_hl_fg="colour6"
+
+# Square theme
+export square_nm_bg=$color16
+export square_nm_fg=$color6
+export square_hl_bg=$color8
+export square_termite_cursor=$color6
