0e548077d7d928e441f78a4f59dfa0766e14941b
Diego Blanco <dbestevez@protonmail.com>
2025-05-13 08:58:39 +0200

    Remove old themes, update flat and update colors

--- colors/nord.sh
+++ colors/nord.sh
@@ -18,42 +18,17 @@ export color16="#2e3440" # background0
 export color17="#3b4252" # background1
 export color18="#434c5e" # background2
 export color19="#4c566a" # background3
-export color20="#5b657b" # foreground012
-export color21="#81a1c1" # foreground3
-export color22="#81a1c1" # cursor
-export color23="#5b657b" # comments
-export color24="#d08770" # warning
-
-# Air theme
-export air_bg=$color16
-export air_danger=$color5
-export air_cursor=$color4
-export air_fg=$color4
-export air_hl=$color4
-export air_info=$color4
-export air_mt=$color0
-export air_separator=$color0
-export air_warning=$color3
-export air_tmux_fg_hl="colour4"
-
-# Future theme
-export future_hl_bg=$color8
-export future_hl_fg=$color4
-export future_mt_bg=$color16
-export future_mt_fg=$color16
-export future_nm_bg=$color8
-export future_nm_fg=$color7
-export future_kitty_cursor=$color6
-export future_tmux_hl_fg="colour4"
-
-# Square theme
-export square_nm_bg=$color16
-export square_nm_fg=$color7
-export square_hl_bg=$color8
-export square_kitty_cursor=$color6
-export square_rofi_hl_bg=$color0
-export square_rofi_hl_fg=$color4
-export square_rofi_nm_bg=$color16
-export square_rofi_nm_fg=$color7
-export square_tmux_nm_bg="colour0"
-export square_tmux_nm_fg="colour7"
+export color20="#5b657b" # foreground0
+export color21="#5b657b" # foreground1
+export color22="#5b657b" # foreground2
+export color23="#81a1c1" # foreground3
+export color24="#8fbcbb" # cursor
+export color25="#5b657b" # border
+export color26="#bf616a" # danger
+export color27="#88c0d0" # info
+export color28="#ebcb8b" # warning
+export color29="#a3be8c" # success
+export color30="#d08770" # comment
+export hyprlandBorderA1="8fbcbb"
+export hyprlandBorderA2="81a1c1"
+export hyprlandBorderI="2e3440"
